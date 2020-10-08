ea2aa2c0d9a19feb86d546dcc758c041ac9a417b
MrEverymorning <60137767+MrEverymorning@users.noreply.github.com>
2020-10-08 01:32:05 -0600

    This better work

---
+++ build/precache-manifest.3a996293f17fed9b1f18d08e1bd8368b.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3993b9db5c96391f1b19ca81e63990a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c089d968d614e3b6c349",
+    "url": "/static/js/duda-widget.js"
+  }
+]);
